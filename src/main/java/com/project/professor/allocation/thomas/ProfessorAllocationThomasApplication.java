e82d005c2829ebd954d77940e9ffcf94585bd19f
Thomas Ribeiro <thomas18rs@gmail.com>
2022-04-27 02:32:42 -0300

    📦 pull e push local remote

---
+++ src/main/java/com/project/professor/allocation/thomas/ProfessorAllocationThomasApplication.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation.thomas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationThomasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationThomasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/professor/allocation/thomas/ProfessorAllocationThomasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation.thomas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessorAllocationThomasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
