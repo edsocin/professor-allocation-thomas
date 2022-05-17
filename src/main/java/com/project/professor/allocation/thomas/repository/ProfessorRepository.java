cd66b8314766e0f3cbb3c96c920fdb1631d8f6f3
Thomas Ribeiro <thomas18rs@gmail.com>
2022-05-17 01:54:29 -0300

    📦 criação das interfaces do repository

---
+++ src/main/java/com/project/professor/allocation/thomas/repository/ProfessorRepository.java
@@ -0,0 +1,17 @@
+package com.project.professor.allocation.thomas.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.project.professor.allocation.thomas.entity.Professor;
+
+@Repository
+public interface ProfessorRepository extends JpaRepository<Professor, Long> {
+
+	List<Professor> findByNameContainingIgnoreCase(String name);
+
+	List<Professor> findByDepartmentId(Long departmentId);
+
+}
