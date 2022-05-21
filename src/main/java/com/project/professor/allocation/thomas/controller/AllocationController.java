9b843903f97c91f9b5e317811cf0670c67399722
Thomas Ribeiro <thomas18rs@gmail.com>
2022-05-21 13:18:09 -0300

    🆙 add allocation controller inicial

--- src/main/java/com/project/professor/allocation/thomas/controller/AllocationController.java
+++ src/main/java/com/project/professor/allocation/thomas/controller/AllocationController.java
@@ -1,5 +1,19 @@
 package com.project.professor.allocation.thomas.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.professor.allocation.thomas.service.AllocationService;
+
+@RestController
+@RequestMapping(path = "/allocations")
 public class AllocationController {
+
+	private final AllocationService allocationService;
+
+	public AllocationController(AllocationService allocationService) {
+		super();
+		this.allocationService = allocationService;
+	}
 
 }
